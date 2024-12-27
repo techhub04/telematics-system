c530053c3c30f01267a0ccbce611b8d82b32ed8e
techhub04 <naushada79@outlook.com>
2024-12-27 19:52:12 +0530

    added pipline and other settings

--- src/services/user-service/repositories/user-repository.js
+++ src/services/user-service/repositories/user-repository.js
@@ -1,7 +1,9 @@
 const User = require("../models/user");
+const {encrypt,decrypt} = require('../../../common/crypto-util');
 
 exports.register = async (userData) => {
     const newUser = new User({...userData});
+    //newUser.password = encrypt(newUser.password);
     newUser = await newUser.save();
     return newUser;
 };
