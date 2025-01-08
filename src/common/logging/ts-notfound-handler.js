9c5cb51881096bc24d50087bd828129fb10ec78f
techhub04 <naushada79@outlook.com>
2025-01-08 23:15:50 +0530

    added code azure pipeline

---
+++ src/common/logging/ts-notfound-handler.js
@@ -0,0 +1,7 @@
+const TSNotFoundHandler = (req,res,next)=>{
+    const error = new Error(`Route ${req.originalUrl} not found`);
+    error.statusCode = 404;
+    next(error);
+}
+
+module.exports = TSNotFoundHandler;
